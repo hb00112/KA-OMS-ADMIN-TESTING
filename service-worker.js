f0bb0f9aeb6618f9d5047e6f6a6946041dce75c2
hb00112 <159218387+hb00112@users.noreply.github.com>
2024-10-13 20:35:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -23,24 +23,3 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
-
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    const options = {
-        body: data.content,
-        icon: 'https://i.postimg.cc/c1nTCZB7/Add-a-subheading.png', // Replace with your own icon
-        badge: 'https://i.postimg.cc/c1nTCZB7/Add-a-subheading.png', // Replace with your own badge
-    };
-
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
-});
-
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    // Handle notification click (e.g., open a specific page)
-    event.waitUntil(
-        clients.openWindow('https://your-app-url.com')
-    );
-});
